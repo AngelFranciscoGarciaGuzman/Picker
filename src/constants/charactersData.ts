8f5fc0b0049cef764065362d682ffc69fcda1ef9
AngelFranciscoGarciaGuzman <angelgarciag14@gmail.com>
2024-11-02 14:02:52 -0600

    Add first character and font

---
+++ src/constants/charactersData.ts
@@ -0,0 +1,90 @@
+export const characterData = [
+    {
+        "name": "The Knight",
+        "description": "The Knight is a Vessel, a mystical insectoid being created by the higher beings The Pale King and The White Lady. They are made of Void and were born in The Abyss, a limbo-like place deep beneath Hallownest."
+    },
+    {
+        "name": "Hornet",
+        "description": ""
+    },
+    {
+        "name": "Quirrel",
+        "description": ""
+    },
+    {
+        "name": "Zote the Mighty",
+        "description": ""
+    },
+    {
+        "name": "The Pale King",
+        "description": ""
+    },
+    {
+        "name": "The Radiance",
+        "description": ""
+    },
+    {
+        "name": "Grimm",
+        "description": ""
+    },
+    {
+        "name": "The Hollow Knight",
+        "description": ""
+    },
+    {
+        "name": "Bretta",
+        "description": ""
+    },
+    {
+        "name": "Cloth",
+        "description": ""
+    },
+    {
+        "name": "Elderbug",
+        "description": ""
+    },
+    {
+        "name": "Tiso",
+        "description": ""
+    },
+    {
+        "name": "Dung Defender",
+        "description": ""
+    },
+    {
+        "name": "Nailmaster Sheo",
+        "description": ""
+    },
+    {
+        "name": "Nailmaster Oro",
+        "description": ""
+    },
+    {
+        "name": "Nailmaster Mato",
+        "description": ""
+    },
+    {
+        "name": "The Hunter",
+        "description": ""
+    },
+    {
+        "name": "Myla",
+        "description": ""
+    },
+    {
+        "name": "Sly",
+        "description": ""
+    },
+    {
+        "name": "Monomon the Teacher",
+        "description": ""
+    },
+    {
+        "name": "Lurien the Watcher",
+        "description": ""
+    },
+    {
+        "name": "Herrah the Beast",
+        "description": ""
+    }
+]

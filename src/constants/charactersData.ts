378fefaee8d7bd3a7e5804e5f3186c66cfbefe8d
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-11-06 17:59:56 -0600

    Functionality of slider

--- src/constants/charactersData.ts
+++ src/constants/charactersData.ts
@@ -1,90 +1,12 @@
 export const characterData = [
     {
+        "id": 1,
         "name": "The Knight",
         "description": "The Knight is a Vessel, a mystical insectoid being created by the higher beings The Pale King and The White Lady. They are made of Void and were born in The Abyss, a limbo-like place deep beneath Hallownest."
     },
     {
+        "id": 2,
         "name": "Hornet",
         "description": "Hornet  is the mysterious self-proclaimed “princess protector” and “guardian” of Hallownest's ruins. Her first appearance was in Hollow Knight, in which she stood as a cold and cruel, ill-tempered foe to The Knight, and is an anti-hero. Hornet is an arachnid warrior equipped with acrobatic skills and is an expert with wielding her weapon of choice, The Needle and Thread."
     },
-    {
-        "name": "Quirrel",
-        "description": ""
-    },
-    {
-        "name": "Zote the Mighty",
-        "description": ""
-    },
-    {
-        "name": "The Pale King",
-        "description": ""
-    },
-    {
-        "name": "The Radiance",
-        "description": ""
-    },
-    {
-        "name": "Grimm",
-        "description": ""
-    },
-    {
-        "name": "The Hollow Knight",
-        "description": ""
-    },
-    {
-        "name": "Bretta",
-        "description": ""
-    },
-    {
-        "name": "Cloth",
-        "description": ""
-    },
-    {
-        "name": "Elderbug",
-        "description": ""
-    },
-    {
-        "name": "Tiso",
-        "description": ""
-    },
-    {
-        "name": "Dung Defender",
-        "description": ""
-    },
-    {
-        "name": "Nailmaster Sheo",
-        "description": ""
-    },
-    {
-        "name": "Nailmaster Oro",
-        "description": ""
-    },
-    {
-        "name": "Nailmaster Mato",
-        "description": ""
-    },
-    {
-        "name": "The Hunter",
-        "description": ""
-    },
-    {
-        "name": "Myla",
-        "description": ""
-    },
-    {
-        "name": "Sly",
-        "description": ""
-    },
-    {
-        "name": "Monomon the Teacher",
-        "description": ""
-    },
-    {
-        "name": "Lurien the Watcher",
-        "description": ""
-    },
-    {
-        "name": "Herrah the Beast",
-        "description": ""
-    }
 ]
